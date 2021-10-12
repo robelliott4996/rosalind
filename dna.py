fc38b2dfcf1aece53d378d0b25369c4ea156280c
robelliott4996 <72887097+robelliott4996@users.noreply.github.com>
2021-10-12 15:09:39 -0400

    Update and rename DNA to dna.py

---
+++ dna.py
@@ -0,0 +1,27 @@
+#for DNA in Rosalind project in python
+import Bio.SeqIO
+
+id = ''
+gc_count = 0
+
+for record in Bio.SeqIO.parse("rosalind_gc.txt", "fasta"):
+    g = 0
+    c = 0
+    a = 0
+    t = 0
+    nucs = 0
+    for nuc in record.seq:
+        if nuc == "G":
+            g += 1
+            nucs += 1
+        if nuc == "C":
+            c += 1
+            nucs += 1
+        if nuc == "A":
+            a += 1
+            nucs += 1
+        if nuc == "T":
+            t += 1
+            nucs += 1
+            
+            
