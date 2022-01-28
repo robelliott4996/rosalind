0d3e4556f6908e9e07eac5c592851dacbc94b79c
robelliott4996 <robelliott4996@gmail.com>
2022-01-28 16:47:34 -0500

    started cons.py and started some basics

--- cons.py
+++ cons.py
@@ -2,4 +2,11 @@
 #provided in fasta format
 
 import Bio.SeqIO
+entrydict = {} #for storing the fasta sequence, May not need this if I only have to iterate through each sequence once
+profdict = {"A":[], "C":[], "T":[],"G":[]} #Keys are nucleotides. object is list of sequence position
+for record in Bio.SeqIO.parse('rosalind_cons.txt', "fasta"):
+    for n in range(len(record.seq)):
 
+
+#for key in entrydict:
+#    print(entrydict[key])
