596918d29de99c14cdcf613272ff48cfc4eb65d5
robelliott4996 <robelliott4996@gmail.com>
2022-01-27 20:11:34 -0500

    what the hell is this problem??

--- prtm.py
+++ prtm.py
@@ -2,11 +2,11 @@
 #calculate a proteins mass from it's sequence.
 #not fasta format
 def aa_mass_lookup():
-    return {'A':71.03711, 'C':103.00919, 'D':115.02694, 'E':129.04259,
-            'F':147.06841, 'G':57.02146, 'H':137.05891, 'I':113.05891,
-            'K':128.09496, 'L':113.08406, 'M':131.04049, 'N':114.04293,
-            'P':97.05276, 'Q':128.05858, 'R':156.10111, 'S':87.03203,
-            'T':101.04768, 'V':99.06841, 'W':186.07931, 'Y':163.06333}
+    return {'A': 71.03711, 'C': 103.00919, 'D': 115.02694, 'E': 129.04259,
+            'F': 147.06841, 'G': 57.02146, 'H': 137.05891, 'I': 113.05891,
+            'K': 128.09496, 'L': 113.08406, 'M': 131.04049, 'N': 114.04293,
+            'P': 97.05276, 'Q': 128.05858, 'R': 156.10111, 'S': 87.03203,
+            'T': 101.04768, 'V': 99.06841, 'W': 186.07931, 'Y': 163.06333}
 seq = ''
 try:
     with open("rosalind_prtm.txt", 'r') as my_file:
@@ -20,7 +20,7 @@ prtm = float(0)
 for n in seq:
     prtm = prtm + float(massdict[n])
 
-#print("{:.5f}".format(prtm))
+print(prtm)
 
 weight = sum([massdict[i] for i in seq])
-print(weight)
+#print(round(weight,2))
