17127c03194ca53b5980c37eb61462e265022406
robelliott4996 <robelliott4996@gmail.com>
2022-02-07 15:21:16 -0500

    complete iprb

--- iprb.py
+++ iprb.py
@@ -1,10 +1,22 @@
 #mendelien inheritance and probability
-
-
-
-
+#Probability two random organisms mate and produce individual
+#with a dominant allele
 try:
-    with open("rosalind_iprb_sample.txt", "r") as my_file:
-
+    with open("rosalind_iprb.txt", "r") as my_file:
+        lines = my_file.readlines()
+        values = lines[0].split(' ') #list with each individual value
 except IOError as err:
     print(err)
+a = float(values[0])  #individuals homo dominant
+b = float(values[1]) #individuals hetero
+c = float(values[2]) #individuals homo recessive
+
+def prob_dom(k,m,n):
+    t = k+m+n
+    pk = k/t
+    pm = m/t
+    pn = n/t
+    prob = 1 - pm*((m-1)/(t-1))*0.25 - 2*pn*(m/(t-1))*0.5 - pn*((n-1)/(t-1))
+
+    return prob
+print(prob_dom(a,b,c))
