c2385ad5815ee929b67eff17b758aca10f0f2603
robelliott4996 <robelliott4996@gmail.com>
2022-02-09 13:17:14 -0500

    start perm.py

--- perm.py
+++ perm.py
@@ -1,2 +1,12 @@
 #given a positive integer n, return the number of possible permutations of the numbers 1->n and list them
 
+try:
+    with open("rosalind_perm.txt", "r") as my_file:
+        lines = my_file.readlines()
+        num = lines[0].rstrip()
+except IOError as err:
+    print(err)
+
+print(num)
+for n in range(1,6):
+    print(n)
