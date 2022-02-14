92635edf65ad2834d5f0371ee7ea5bba350aea6e
robelliott4996 <robelliott4996@gmail.com>
2022-02-14 15:37:20 -0500

    completed grph.py

--- grph.py
+++ grph.py
@@ -5,7 +5,20 @@
 import Bio.SeqIO #to read fasta file
 
 entrydict = {}
+resultlist = []
 for record in Bio.SeqIO.parse('rosalind_grph.txt', "fasta"):
     entrydict[record.id] = record.seq
+    #print(record.id, record.seq)
+for first in entrydict:
+    for second in entrydict:
+        if entrydict[first][-3:] == entrydict[second][:3:] and first != second:
+            #insert into resultslists
+            resultlist.append((first, second))
 
+        else:
+            continue
 #store results in a dictionary for printing.
+for result in resultlist:
+    print("{} {}".format(result[0], result[1]))
+
+    #results were very long, should've printed to a file
