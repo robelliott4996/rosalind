00d53beacc6ef52f8f7379e5c85a42fb30e8593b
robelliott4996 <robelliott4996@gmail.com>
2022-02-15 09:42:05 -0500

    begin long.py

---
+++ long.py
@@ -0,0 +1,8 @@
+#given a collection of strings, find and create the single superstring
+#fasta format. strings are all the same length
+#string overlap will be more than half their length
+
+
+
+#if len(overlap) is > (half the length of the substrings) and :
+    #call function
